fda17af130200e9c568824381475e46e9f57e9dd
Akul G Devali <64837282+akuldevali@users.noreply.github.com>
2024-09-12 01:19:45 +0000

    Made files modular

--- sort/rand.py
+++ sort/rand.py
@@ -4,7 +4,7 @@ import subprocess
 def random_array(arr):
     """Random number generator"""
     shuffled_num = None
-    for i in range(len(arr)):
+    for i in enumerate(len(arr)):
         shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
         arr[i] = int(shuffled_num.stdout)
     return arr
