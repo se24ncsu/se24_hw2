6bbfc7da29a18786e78c186145dc0e4c722d3118
Srimadh V Rao <64469917+Shrimadh@users.noreply.github.com>
2024-09-12 15:49:17 +0000

    Fixed logical issues in MergeSort func

--- source/hw2_debugging.py
+++ source/hw2_debugging.py
@@ -2,7 +2,7 @@
 
 def merge_sort(arr):
     """A start-up function for merge sort"""
-    if len(arr) == 1:
+    if len(arr) <= 1:
         return arr
 
     half = len(arr)//2
@@ -13,19 +13,21 @@ def recombine(left_arr, right_arr):
     """ A function to recombine all individual elements in a sorted manner"""
     left_index = 0
     right_index = 0
-    merge_arr = [None] * (len(left_arr) + len(right_arr))
+    merge_arr = [0] * (len(left_arr) + len(right_arr))
     while left_index < len(left_arr) and right_index < len(right_arr):
         if left_arr[left_index] < right_arr[right_index]:
-            right_index += 1
             merge_arr[left_index + right_index] = left_arr[left_index]
-        else:
             left_index += 1
+        else:
             merge_arr[left_index + right_index] = right_arr[right_index]
+            right_index += 1
 
-    for i in range(right_index, len(right_arr)):
-        merge_arr[left_index + right_index] = right_arr[i]
+    while right_index < len(right_arr):
+        merge_arr[left_index + right_index] = right_arr[right_index]
+        right_index += 1
 
-    for i in range(left_index, len(left_arr)):
-        merge_arr[left_index + right_index] = left_arr[i]
+    while left_index < len(left_arr):
+        merge_arr[left_index + right_index] = left_arr[left_index]
+        left_index += 1
 
     return merge_arr
