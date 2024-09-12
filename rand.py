ba631667c3ac82691ae098237a50eff03dfbea9a
Akul G Devali <64837282+akuldevali@users.noreply.github.com>
2024-09-12 01:15:34 +0000

    Fixed all snake_case warning

--- rand.py
+++ rand.py
@@ -2,6 +2,7 @@
 import subprocess
 
 def random_array(arr):
+    """Random number generator"""
     shuffled_num = None
     for i in range(len(arr)):
         shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
